4cabb18080deb11bc3913114ae1f46b64019bb74
Gilberto Martini <gilberto.martini@e-hps.com>
2025-07-05 12:21:59 -0300

    estabilishing connection to mongodb atlas

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotnev from 'dotenv';
+import { connectDB } from './config/db.js';
 
 dotnev.config(); //reads .env file and adds the keys into process.env so we can retrieve its values)
 
@@ -13,4 +14,7 @@ console.log(process.env.MONGO_URI);
 
 const port = 5000;
 
-app.listen(port, () => { console.log(`App running on http://localhost:${port} hello`) });
+app.listen(port, () => {
+    connectDB();
+    console.log(`App running on http://localhost:${port} hello`)
+});
