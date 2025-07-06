a8dbce0dc0829b3f67a996a80b5c1b7c8492b514
Gilberto Martini <gilberto.martini@e-hps.com>
2025-07-05 23:13:45 -0300

    refactor of handlers

---
+++ backend/handlers/product.handler.js
@@ -0,0 +1,85 @@
+import Product from '../models/Product.js'
+import mongoose from 'mongoose';
+
+export const getProductById = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const product = await Product.findById(id);
+        if (!product) {
+            res.status(204).json({ success: false, message: 'Product not found' });
+        }
+        res.status(200).json({ success: true, data: product });
+
+    } catch (error) {
+        console.log(`Error while fetching product`,)
+        res.status(500).json({ success: false, message: 'Server Error' });
+    }
+};
+
+export const getAllProducts = async (_, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({ success: true, data: products });
+
+    } catch (error) {
+        console.log(`Error while fetching products`,)
+        res.status(400).json({ success: false, message: 'Server Error' });
+    }
+}
+
+export const createNewProduct = async (req, res) => {
+    const product = await req.body;
+
+    if (!product) return res.status(400).json({ success: false, message: 'empty body' });
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: 'Please provide all fields' });
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+
+        res.status(201)
+            .json({ success: false, data: newProduct })
+            ;
+    } catch (error) {
+        console.error("Error during save operation: ", error);
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+};
+
+export const updateProduct = async (req, res) => {
+    const { id } = req.params;
+
+    const product = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        res.status(400).json({ success: false, message: 'Invalid id' });
+    }
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
+        res.status(204).json({ success: true, message: `product with id ${id} updated successfully`, data: updatedProduct });
+
+    } catch (error) {
+        console.log(`Error while trying to update the product: ${error.message} `);
+        res.status(500).json({ success: false, message: 'Server Error' });
+    }
+
+};
+
+export const deleteProduct = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(204).send();
+    } catch (error) {
+        console.error(`Error while trying to delete product with id: ${id}`);
+        res.status(500).json({ success: false, message: `could not delete product with id: ${id}` });
+    }
+
+};
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,92 +1,14 @@
-import { express } from "express";
-import Product from './models/Product.js';
-import mongoose from 'mongoose';
+import express from "express";
+
+import { createNewProduct, getAllProducts, getProductById, updateProduct, deleteProduct } from "../handlers/product.handler.js";
 
 const router = express.Router();
 
-
-router.get("/:id", async (req, res) => {
-    const { id } = req.params;
-
-    try {
-        const product = await Product.findById(id);
-        if (!product) {
-            res.status(204).json({ success: false, message: 'Product not found' });
-        }
-        res.status(200).json({ success: true, data: product });
-
-    } catch (error) {
-        console.log(`Error while fetching product`,)
-        res.status(500).json({ success: false, message: 'Server Error' });
-    }
-});
-
-router.get("/", async (_, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json({ success: true, data: products });
-
-    } catch (error) {
-        console.log(`Error while fetching products`,)
-        res.status(400).json({ success: false, message: 'Server Error' });
-    }
-});
-
-router.post("/", async (req, res) => {
-    const product = await req.body;
-
-    if (!product) return res.status(400).json({ success: false, message: 'empty body' });
-
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({ success: false, message: 'Please provide all fields' });
-    }
-
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-
-        res.status(201)
-            .json({ success: false, data: newProduct })
-            ;
-    } catch (error) {
-        console.error("Error during save operation: ", error);
-        res.status(500).json({ success: false, message: 'Internal server error' });
-    }
-});
-
-router.put("/:id", async (req, res) => {
-    const { id } = req.params;
-
-    const product = req.body;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        res.status(400).json({ success: false, message: 'Invalid id' });
-    }
-
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
-        res.status(204).json({ success: true, message: `product with id ${id} updated successfully`, data: updatedProduct });
-
-    } catch (error) {
-        console.log(`Error while trying to update the product: ${error.message} `);
-        res.status(500).json({ success: false, message: 'Server Error' });
-    }
-
-});
-
-router.delete("/:id", async (req, res) => {
-    const { id } = req.params;
-
-    try {
-        await Product.findByIdAndDelete(id);
-        res.status(204).send();
-    } catch (error) {
-        console.error(`Error while trying to delete product with id: ${id}`);
-        res.status(500).json({ success: false, message: `could not delete product with id: ${id}` });
-    }
-
-});
+router.get("/:id", getProductById);
+router.get("/", getAllProducts);
+router.post("/", createNewProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 
 export default router;
